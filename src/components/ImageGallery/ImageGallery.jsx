10304781dff6bccf590af4bc0ff3e132b08393e3
Oksana <kskaplun@gmail.com>
2022-05-29 12:16:25 +0300

    refactoring gallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,55 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './ImageGallery.module.css';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Modal from 'components/Modal/Modal';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
-export default class ImageGallery extends Component {
-  state = {
-    showModal: false,
-    bigPic: null,
-  };
+export default function ImageGallery({ images }) {
+  const [showModal, setShowModal] = useState(false);
+  const [bigPic, setBigPic] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     document.addEventListener('click', e => {
       if (e.target.nodeName !== 'IMG') {
-        this.setState({ showModal: false });
         return;
-      } else {
-        let picture = this.props.images.filter(obj => {
-          return obj.id === parseInt(e.target.alt);
-        });
-        this.setState({ bigPic: picture[0].largeImageURL });
       }
+      let picture = images.filter(obj => {
+        return obj.id === parseInt(e.target.alt);
+      });
+      if (!picture.length) {
+        return;
+      }
+      setBigPic(picture[0].largeImageURL);
     });
-  }
+  }, [bigPic, images]);
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  const toggleModal = () => {
+    setShowModal(prevShow => !prevShow);
   };
 
-  render() {
-    const { showModal, bigPic } = this.state;
-    return (
-      <>
-        <ul className={s.gallery} onClick={this.toggleModal}>
-          {this.props.images.map(img => {
-            return (
-              <ImageGalleryItem
-                key={nanoid()}
-                smallImgURL={img.webformatURL}
-                id={img.id}
-              />
-            );
-          })}
-        </ul>
-        {showModal && bigPic && (
-          <Modal onClose={this.toggleModal} pic={bigPic} />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <ul className={s.gallery} onClick={toggleModal}>
+        {images.map(img => {
+          return (
+            <ImageGalleryItem
+              key={nanoid()}
+              smallImgURL={img.webformatURL}
+              id={img.id}
+            />
+          );
+        })}
+      </ul>
+      {showModal && bigPic && <Modal onClose={toggleModal} pic={bigPic} />}
+    </>
+  );
 }
 
 ImageGallery.propTypes = {
